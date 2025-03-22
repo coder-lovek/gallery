5f54dcf84714f32c2ce161836367ffb9aedc0429
Love <lovekumar.200442@gmail.com>
2025-03-22 18:46:07 +0530

    complete

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 function Card({ image }) {
-    const handleSubmit=()=>{
-        
-    }
-  return (
-    <div className="card">
-      <img src={image.webformatURL} alt="Result" />
-      <div className="card-info">
-        <p> likes-{image.likes}</p>
-        <button onClick={handleSubmit}>download</button>
-      </div>
-    </div>
-  );
+    const handleDownload =  (imageSrc) => {
+        window.open(imageSrc, "_blank");
+    };
+
+    return (
+        <div className="card">
+            <img src={image.webformatURL} alt="Result" />
+            <div className="card-info">
+                <p>Likes: {image.likes}</p>
+                <button onClick={() => handleDownload(image.largeImageURL)}>
+                    View
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
